52f3fbedb40a60a47dc07085c2466b6f5441b2e0
admin <admin@map-computer.local>
2023-06-03 15:10:10 -0400

    testing

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
-var map = L.map('map').setView([40.7128, -74.0060], 11);
+var map = L.map('map').setView([40.7128, -74.0060], 10);
 
 // Add the tile layer
 L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}@2x.png').addTo(map);
 
-var geojsonLayer; // Store the GeoJSON layer
+// Create a marker cluster group
+var markerCluster = L.markerClusterGroup();
 
-// Load the GeoJSON data and add it to the map
+var data; // Declare the data variable
+
+// Load the GeoJSON data and add it to the marker cluster group
 fetch('https://raw.githubusercontent.com/evanapplegate/testpile/main/2022_crashes3.geojson')
   .then(function (response) {
     return response.json();
   })
-  .then(function (data) {
+  .then(function (jsonData) {
+    // Store the GeoJSON data in the data variable
+    data = jsonData;
+
+    // Create a custom icon for the blue markers
+    var blueIcon = L.divIcon({
+      className: 'blue-icon',
+      iconSize: [6, 6],
+    });
+
+    // Create a custom icon for the red markers
+    var redIcon = L.divIcon({
+      className: 'red-icon',
+      iconSize: [8, 8],
+    });
+
     // Create a custom style function for the GeoJSON layer
     function style(feature) {
       var numberOfPersonsKilled = feature.properties['NUMBER OF PERSONS KILLED'];
-      var fillColor = numberOfPersonsKilled > 0 ? '#ac452d' : '#e5bd5d';
+      var fillColor = numberOfPersonsKilled > 0 ? 'red' : 'orange';
       var fillOpacity = numberOfPersonsKilled > 0 ? 1 : 0.2;
       var radius = numberOfPersonsKilled > 0 ? 4 : 3;
       return {
@@ -22,16 +40,17 @@ fetch('https://raw.githubusercontent.com/evanapplegate/testpile/main/2022_crashe
         fillColor: fillColor,
         fillOpacity: fillOpacity,
         color: fillColor,
-        weight: .2,
+        weight: 1,
         riseOnHover: true, // Ensure points appear on top on hover
       };
     }
 
     // Create a GeoJSON layer with custom styling and tooltip
-    geojsonLayer = L.geoJSON(data, {
-      style: style,
+    var geojsonLayer = L.geoJSON(data, {
       pointToLayer: function (feature, latlng) {
-        return L.circleMarker(latlng);
+        var numberOfPersonsKilled = feature.properties['NUMBER OF PERSONS KILLED'];
+        var icon = numberOfPersonsKilled > 0 ? redIcon : blueIcon;
+        return L.marker(latlng, { icon: icon });
       },
       onEachFeature: function (feature, layer) {
         var tooltipContent =
@@ -39,61 +58,68 @@ fetch('https://raw.githubusercontent.com/evanapplegate/testpile/main/2022_crashe
           '<br>Deaths: ' + feature.properties['NUMBER OF PERSONS KILLED'];
         layer.bindTooltip(tooltipContent);
       },
-    }).addTo(map);
-  });
+    });
 
-    // Create a legend
-    var legend = L.control({ position: 'topright' });
-    legend.onAdd = function (map) {
-      var div = L.DomUtil.create('div', 'legend');
-      div.innerHTML +=
-        '<div><span class="legend-circle" style="background-color: red;"></span>Crash with deaths</div>' +
-        '<div><span class="legend-circle" style="background-color: orange;"></span>Crash with injuries only</div>';
-      return div;
-    };
-    legend.addTo(map);
+    // Add the GeoJSON layer to the marker cluster group
+    markerCluster.addLayer(geojsonLayer);
 
+    // Add the marker cluster group to the map
+    map.addLayer(markerCluster);
 
-// Add a time slider
-var slider = document.getElementById('time-slider');
+    // Add a time slider
+    var slider = document.getElementById('time-slider');
 
-noUiSlider.create(slider, {
-  start: [0, 24], // Initial time range
-  connect: true,
-  step: 1,
-  range: {
-    min: 0,
-    max: 24,
-  },
-  tooltips: true,
-  pips: {
-    mode: 'positions',
-    values: [0, 100],
-    density: 4,
-    format: {
-      to: function (value) {
-        if (value === 0) return '12a';
-        if (value === 24) return '11:59p';
-        return value + 'h';
+    noUiSlider.create(slider, {
+      start: [0, 24], // Initial time range
+      connect: true,
+      step: 1,
+      range: {
+        min: 0,
+        max: 24,
       },
-    },
-  },
-});
+      tooltips: true,
+    });
 
-slider.noUiSlider.on('update', function (values, handle) {
-  // Filter the GeoJSON layer based on the time range
-  var timeRange = values.map(function (value) {
-    return parseInt(value);
-  });
+    slider.noUiSlider.on('update', function (values, handle) {
+      // Filter the GeoJSON layer based on the time range
+      var timeRange = values.map(function (value) {
+        return parseInt(value);
+      });
 
-  geojsonLayer.eachLayer(function (layer) {
-    var crashTime = parseInt(layer.feature.properties['CRASH TIME'].split(':')[0]);
-    if (crashTime >= timeRange[0] && crashTime <= timeRange[1]) {
-      layer.setStyle({ opacity: 1 });
-      layer.addTo(map);
-    } else {
-      layer.setStyle({ opacity: 0 });
-      map.removeLayer(layer);
-    }
+      // Remove the GeoJSON layer from the map
+      map.removeLayer(markerCluster);
+
+      // Clear the marker cluster group
+      markerCluster.clearLayers();
+
+      // Create a new filtered GeoJSON layer
+      var filteredGeojsonLayer = L.geoJSON(data, {
+        filter: function (feature) {
+          var crashTime = feature.properties['CRASH TIME'];
+          var hours = parseInt(crashTime.split(':')[0]);
+          return hours >= timeRange[0] && hours <= timeRange[1];
+        },
+        style: style,
+        pointToLayer: function (feature, latlng) {
+          var numberOfPersonsKilled = feature.properties['NUMBER OF PERSONS KILLED'];
+          var icon = numberOfPersonsKilled > 0 ? redIcon : blueIcon;
+          return L.marker(latlng, { icon: icon });
+        },
+        onEachFeature: function (feature, layer) {
+          var tooltipContent =
+            'Injured: ' + feature.properties['NUMBER OF PERSONS INJURED'] +
+            '<br>Deaths: ' + feature.properties['NUMBER OF PERSONS KILLED'];
+          layer.bindTooltip(tooltipContent);
+        },
+      });
+
+      // Add the filtered GeoJSON layer to the marker cluster group
+      markerCluster.addLayer(filteredGeojsonLayer);
+
+      // Add the marker cluster group back to the map
+      map.addLayer(markerCluster);
+    });
+  })
+  .catch(function (error) {
+    console.log('Error:', error);
   });
-});
